72e3fa145a467229ba6286e7b07c07df503c8603
olaide-hok <olaidekareem43@yahoo.com>
2022-02-16 14:02:46 +0100

    create GithubContext and wrapped the return elements with the GithubProvider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { GithubProvider } from "./components/context/github/GithubContext";
 import Footer from "./components/layouts/Footer";
 import Navbar from "./components/layouts/Navbar";
 import About from "./components/pages/About";
@@ -7,20 +8,22 @@ import NotFound from "./components/pages/NotFound";
 
 function App() {
   return (
-   <Router>
-     <div className="flex flex-col justify-between h-screen">
-       <Navbar />
-       <main className="container mx-auto px-3 pb-12">
-         <Routes>
-           <Route path='/' element={<Home />} />
-           <Route path='/about' element={<About />} />
-           <Route path='/notfound' element={<NotFound />} />
-           <Route path='/*' element={<NotFound />} />
-         </Routes>
-       </main>
-       <Footer />
-     </div>
-   </Router>
+    <GithubProvider>    
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/notfound' element={<NotFound />} />
+              <Route path='/*' element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+   </GithubProvider>
   );
 }
 
