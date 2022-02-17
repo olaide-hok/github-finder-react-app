7a1fac733de294f5185f25ec5b12531ff06470d7
olaide-hok <olaidekareem43@yahoo.com>
2022-02-17 10:31:38 +0100

    use useReducer to replace useState and clean up the fetchUser function

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,15 +1,10 @@
-import { useEffect, useContext } from 'react'
+import { useContext } from 'react'
 import GithubContext from '../context/github/GithubContext'
 import Spinner from '../layouts/Spinner'
 import UserItem from './UserItem'
 
 function UserResults() {  
-    const {users, loading, fetchUsers} = useContext(GithubContext) 
-
-    useEffect(() => {
-        fetchUsers()
-
-    }, [])
+    const {users, loading} = useContext(GithubContext)
 
     if(!loading) {
         return (
